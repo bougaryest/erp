21bc2cc023fd6d97fc72a228199a316c2d9b9b56
Omar Nabil <omar.nabil@crevisoft.com>
2023-05-15 16:22:10 +0300

    add e-invoice branch

---
+++ invoice_edit_report/__manifest__.py
@@ -0,0 +1,22 @@
+
+
+{
+    'name': 'E-invoice add branch',
+    'version': '15.0.2.2.2',
+    'category': 'Accounting',
+    'live_test_url': 'https://www.youtube.com/watch?v=peAp2Tx_XIs',
+    'summary': """ """,
+    'description': """
+        Adding field branch to tax invoice  report
+                    """,
+    'author': '',
+    'depends': ['electronic_invoice'],
+    'data': [
+        'report/edit_report.xml',
+    ],
+
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
